3072bdf6418ef9687f1a0dc80fc4c7c4763faa15
Liem Nguyen <40646564+daniel-liemng@users.noreply.github.com>
2020-12-28 02:27:34 -0500

    Login, Register Pages

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,7 +1,124 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Paper from "@material-ui/core/Paper";
+import TextField from "@material-ui/core/TextField";
+import { Button, Typography } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+  paper: {
+    marginTop: "3rem",
+    padding: "1rem",
+  },
+  title: {
+    marginTop: "1rem",
+    marginBottom: "1rem",
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(2),
+  },
+  submitBtn: {
+    marginTop: "1.5rem",
+    marginBottom: "2rem",
+  },
+  link: {
+    textDecoration: "none",
+    marginLeft: "1rem",
+
+    "&:hover": {
+      color: theme.palette.primary.main,
+    },
+  },
+}));
 
 const Login = () => {
-  return <div>Login</div>;
+  const classes = useStyles();
+
+  const [userInput, setUserInput] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setUserInput({ ...userInput, [e.target.name]: e.target.value });
+  };
+  const handleLogin = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <Container maxWidth='sm'>
+      <Paper elevation={0} className={classes.paper}>
+        <Typography
+          variant='h4'
+          align='center'
+          className={classes.title}
+          color='primary'
+        >
+          Login
+        </Typography>
+
+        <form
+          className={classes.root}
+          noValidate
+          autoComplete='off'
+          onClick={handleLogin}
+        >
+          <TextField
+            label='Email'
+            placeholder='name@email.com'
+            fullWidth
+            style={{ margin: 8 }}
+            margin='normal'
+            InputLabelProps={{
+              shrink: true,
+            }}
+            name='email'
+            value={userInput.email}
+            onChange={handleChange}
+          />
+
+          <TextField
+            type='password'
+            label='Password'
+            placeholder='Password'
+            fullWidth
+            style={{ margin: 8 }}
+            margin='normal'
+            InputLabelProps={{
+              shrink: true,
+            }}
+            name='password'
+            value={userInput.password}
+            onChange={handleChange}
+          />
+
+          <Button
+            type='submit'
+            variant='contained'
+            color='primary'
+            className={classes.submitBtn}
+          >
+            Login
+          </Button>
+        </form>
+
+        <Typography variant='h6'>
+          Don't have an account?{" "}
+          <Link to='/register' className={classes.link}>
+            Register
+          </Link>
+        </Typography>
+      </Paper>
+    </Container>
+  );
 };
 
 export default Login;
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,7 +1,139 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Paper from "@material-ui/core/Paper";
+import TextField from "@material-ui/core/TextField";
+import { Button, Typography } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+  paper: {
+    marginTop: "3rem",
+    padding: "1rem",
+  },
+  title: {
+    marginTop: "1rem",
+    marginBottom: "1rem",
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(2),
+  },
+  submitBtn: {
+    marginTop: "1.5rem",
+    marginBottom: "2rem",
+  },
+  link: {
+    textDecoration: "none",
+    marginLeft: "1rem",
+
+    "&:hover": {
+      color: theme.palette.primary.main,
+    },
+  },
+}));
 
 const Register = () => {
-  return <div>Register</div>;
+  const classes = useStyles();
+
+  const [userInput, setUserInput] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setUserInput({ ...userInput, [e.target.name]: e.target.value });
+  };
+  const handleLogin = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <Container maxWidth='sm'>
+      <Paper elevation={0} className={classes.paper}>
+        <Typography
+          variant='h4'
+          align='center'
+          className={classes.title}
+          color='primary'
+        >
+          Register
+        </Typography>
+
+        <form
+          className={classes.root}
+          noValidate
+          autoComplete='off'
+          onClick={handleLogin}
+        >
+          <TextField
+            label='Name'
+            placeholder='Name'
+            fullWidth
+            style={{ margin: 8 }}
+            margin='normal'
+            InputLabelProps={{
+              shrink: true,
+            }}
+            name='name'
+            value={userInput.name}
+            onChange={handleChange}
+          />
+
+          <TextField
+            label='Email'
+            placeholder='name@email.com'
+            fullWidth
+            style={{ margin: 8 }}
+            margin='normal'
+            InputLabelProps={{
+              shrink: true,
+            }}
+            name='email'
+            value={userInput.email}
+            onChange={handleChange}
+          />
+
+          <TextField
+            type='password'
+            label='Password'
+            placeholder='Password'
+            fullWidth
+            style={{ margin: 8 }}
+            margin='normal'
+            InputLabelProps={{
+              shrink: true,
+            }}
+            name='password'
+            value={userInput.password}
+            onChange={handleChange}
+          />
+
+          <Button
+            type='submit'
+            variant='contained'
+            color='primary'
+            className={classes.submitBtn}
+          >
+            Register
+          </Button>
+        </form>
+
+        <Typography variant='h6'>
+          Already have an account?{" "}
+          <Link to='/login' className={classes.link}>
+            Login
+          </Link>
+        </Typography>
+      </Paper>
+    </Container>
+  );
 };
 
 export default Register;
